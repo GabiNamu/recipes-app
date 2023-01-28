a9fe2fd4aa3de25c47a18302a17a427c6395132a
Gabriela Muniz <gabrielanamu@live.com>
2023-01-28 17:36:02 -0300

    adiciona req 21 e 22

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import RecipeCard from '../components/RecipeCard';
 import { getRecipes } from '../context/api/mealDB';
+import { Context } from '../context/provider/ApiProvider';
 
 function Recipes() {
   const history = useHistory();
+  const { setRecipeList } = useContext(Context);
+  const [dobleClick, setDobleClick] = useState({ name: '' });
   const [categorys, setCategorys] = useState({});
   const heading = history.location.pathname === '/meals' ? 'Meals' : 'Drinks';
   const recipeObj = heading.toLowerCase();
@@ -14,9 +17,7 @@ function Recipes() {
     const fetchCategorys = async () => {
       if (recipeObj === 'meals') {
         const resultCategorys = await getRecipes('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
-        console.log(resultCategorys);
         const cat = resultCategorys.meals.slice(0, Number('5'));
-        console.log({ meals: cat });
         setCategorys({ meals: cat });
         return;
       }
@@ -27,6 +28,45 @@ function Recipes() {
     fetchCategorys();
   }, []);
 
+  const handleClickCategory = async (e) => {
+    if (recipeObj === 'meals') {
+      if (dobleClick.name === e.target.id) {
+        const resultMeals = await getRecipes('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+        setRecipeList(resultMeals);
+        setDobleClick({ name: '' });
+        return;
+      }
+      const result = await getRecipes(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${e.target.id}`);
+      setRecipeList(result);
+      console.log(dobleClick.name);
+      console.log(e.target.id);
+      setDobleClick({ name: e.target.id });
+    }
+    if (recipeObj === 'drinks') {
+      if (dobleClick.name === e.target.id) {
+        const resultDrinks = await getRecipes('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        setRecipeList(resultDrinks);
+        setDobleClick({ name: '' });
+        return;
+      }
+      const result = await getRecipes(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${e.target.id}`);
+      setRecipeList(result);
+      setDobleClick({ name: e.target.id });
+    }
+  };
+
+  const handleClickAll = async () => {
+    if (recipeObj === 'meals') {
+      const resultMeals = await getRecipes('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      setRecipeList(resultMeals);
+      return;
+    }
+    if (recipeObj === 'drinks') {
+      const resultDrinks = await getRecipes('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      setRecipeList(resultDrinks);
+    }
+  };
+
   return (
     <div>
       <Header title={ heading } search />
@@ -34,11 +74,21 @@ function Recipes() {
         <button
           type="button"
           key={ category.strCategory }
+          id={ category.strCategory }
           data-testid={ `${category.strCategory}-category-filter` }
+          onClick={ handleClickCategory }
         >
           {category.strCategory}
         </button>
       ))}
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ handleClickAll }
+      >
+        All
+
+      </button>
       <RecipeCard recipeObj={ recipeObj } />
     </div>
   );
