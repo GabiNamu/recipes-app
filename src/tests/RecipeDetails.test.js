68031c6db29cb5c83574bd6d3b80830de5a5aada
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-01-30 18:59:05 -0300

    req 29 faltando só a cobertura de testes

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
