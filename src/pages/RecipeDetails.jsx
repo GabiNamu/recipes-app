35d791dabbef6f24d6854008fb34248633ee4bb4
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-01-31 18:19:39 -0300

    req 31 ok e lógica consertada (bug dos ingredientes sem quantidade)

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipeDetailsInterface from '../components/RecipeDetailsInterface';
 
-function RecipeDetails({ match: { path, params: { id } } }) {
+function RecipeDetails({ history, match: { path, params: { id } } }) {
   const [loading, setLoading] = useState(true);
 
   return (
     <div>
-      <RecipeDetailsInterface props={ [loading, setLoading, path, id] } />
+      <RecipeDetailsInterface props={ [loading, setLoading, path, id, history] } />
     </div>
   );
 }
 
 RecipeDetails.propTypes = {
   match: PropTypes.shape(PropTypes.any.isRequired).isRequired,
+  history: PropTypes.shape(PropTypes.any.isRequired).isRequired,
 };
 
 export default RecipeDetails;
