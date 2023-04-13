77e7cb7c00bc664b81f328fe50832e808201df10
Gabriela Muniz <gabrielanamu@live.com>
2023-04-13 18:06:42 -0300

    feat: add gh-pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ApiProvider from './context/provider/ApiProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <ApiProvider>
       <App />
     </ApiProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
 
 // If you want your app to work offline and load faster, you can change
